d32341ced5ace37ab8bbe7e9c0b9c1834e35e84c
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-04-25 16:09:45 -0400

    test code for user flag

--- src/MineSweepTest.java
+++ src/MineSweepTest.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Class description
  *
@@ -16,4 +18,37 @@ public class MineSweepTest {
         testMirrorGrid = new MirrorGrid(10);
     }
 
-}
+    public static void allieTestsHere () {
+
+        HiddenGrid testHiddenGrid;
+        MirrorGrid testMirrorGrid;
+        testHiddenGrid = new HiddenGrid(10);
+        testMirrorGrid = new MirrorGrid(10);
+
+        Scanner scan = new Scanner (System.in);
+
+        int userXtoFlag;
+        int userYtoFlag;
+
+        //is this best way to take [][] in from user? can we do in one line?
+        System.out.println("Enter an X value now: ");
+        userXtoFlag = scan.nextInt();
+
+        System.out.println("Enter a Y value now: ");
+        userYtoFlag = scan.nextInt();
+
+        testMirrorGrid.setMirrorCell(userXtoFlag, userYtoFlag, "!");
+
+        testMirrorGrid.printGridToConsole();
+
+
+    }
+
+
+
+
+
+
+    }
+
+

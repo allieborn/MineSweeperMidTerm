e7b2e70a874c7fcad151a67725d8b9725da2ad70
Alexandra <aborn601722@Alexandras-MacBook-Pro.local>
2017-04-25 13:58:31 -0400

    added s user instructions

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,6 +5,8 @@ public class MainApp {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        String userLetter;
+        int userNumber;
 
         int gridSize;
 
@@ -12,12 +14,21 @@ public class MainApp {
 
         System.out.println("Enter the size of your grid to play on: ");
         gridSize = scan.nextInt();
+        scan.nextLine();
 
-        // getting user input, assumed input is "10"
         HiddenGrid hiddenGrid = new HiddenGrid(gridSize);
         MirrorGrid mirrorGrid = new MirrorGrid(gridSize);
 
         mirrorGrid.printGridToConsole();
+
+        System.out.println("Enter a letter row now: ");
+        userLetter = scan.nextLine();
+
+        System.out.println("Enter a number column: ");
+        userNumber = scan.nextInt(); 
+
+
+
 
     }
 
